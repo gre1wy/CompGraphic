7cef6d5dd8d16f71decf0181860c9d92aca8ca7d
gre1wy <grundayaroslav28@gmail.com>
2024-04-07 15:48:10 +0300

    lab4 gifs with fractals

--- Lab4/lab4.py
+++ Lab4/lab4.py
@@ -1,33 +1,44 @@
 import matplotlib.pyplot as plt
 import numpy as np
 
-def generate_barnsley_fern(rules, iterations=1000, delay=0.01):
+def generate_fractal(rules: dict, iterations: int):
     # Початкові координати
     x, y = [0], [0]
 
-    # Створення графіку
-    fig, ax = plt.subplots()
-    ax.set_aspect('equal')
-    ax.set_xlim(-3, 3)
-    ax.set_ylim(0, 10)
-    plt.title('Dynamic Barnsley Fern')
-
-    # Додавання точок поступово
+    # Генерація фракталу
     for _ in range(iterations):
         rule = np.random.choice(rules)
         x1 = rule['a'] * x[-1] + rule['b'] * y[-1] + rule['e']
         y1 = rule['c'] * x[-1] + rule['d'] * y[-1] + rule['f']
         x.append(x1)
         y.append(y1)
-        ax.plot(x1, y1, '.', color='green', markersize=0.5)
-        plt.draw()
-        plt.pause(delay)
-        plt.show()
+    return x, y
 
-# Виклик функції з вашими наборами правил
+def display_fractal(rules: dict, iterations: int, delay: float, pixels: int):
+    x, y = generate_fractal(rules, iterations)
+    plt.figure()  
+    plt.ion() 
+    annotation = None 
+    for i in range(iterations+1):
+        if i % pixels == 0:
+            if annotation is not None:
+                annotation.remove()
+            plt.plot(x[i:i+pixels], y[i:i+pixels], '*', color='green', markersize=0.5)
+            plt.title('Fractal')
+            annotation = plt.annotate(f'Pixels drawn: {i}', xy=(0, 0), fontsize=10, color='red')
+            plt.draw()
+            plt.pause(delay)
+    plt.ioff()  
+    plt.show()
+
 rules = [
-    {'a': 0.2020, 'b': -0.8050, 'c': -0.6890, 'd': -0.3420, 'e': -0.3730, 'f': -0.6530},
-    {'a': 0.1380, 'b': 0.6650, 'c': -0.5020, 'd': -0.2220, 'e': 0.6600, 'f': -0.2770}
+    {'a': 0.1950, 'b': -0.4880, 'c': 0.3440, 'd': 0.4430, 'e': 0.4431, 'f': 0.2452},
+    {'a': 0.4620, 'b': 0.4140, 'c': -0.2520, 'd': 0.3610, 'e': 0.2511, 'f': 0.5692},
+    {'a': -0.6370, 'b': 0.0000, 'c': 0.0000, 'd': 0.5010, 'e': 0.8562, 'f': 0.2512},
+    {'a': -0.0350, 'b': 0.0700, 'c': -0.4690, 'd': 0.0220, 'e': 0.4884, 'f': 0.5069},
+    {'a': -0.0580, 'b': -0.0700, 'c': 0.4530, 'd': -0.1110, 'e': 0.5976, 'f': 0.0969}
 ]
+# Первый вызов функции display_fractal
+display_fractal(rules, iterations=10000, delay=0.01, pixels=100)
 
-generate_barnsley_fern(rules)
+
